15ecb4f11bee05c84b129a971995ea2a642a6b4f
Lars <behethangames@gmail.com>
2022-12-28 19:11:26 +0100

    Added Program.cs

---
+++ Arch.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Arch.Core;
+using Test;
+
+Console.WriteLine("Hello, World!");
+
+var world = World.Create();
+world.Create(new Position(0,0), new Velocity(1,1));
+world.Create(new Position(0,0), new Velocity(1,1));
+world.Create(new Position(0,0), new Velocity(1,1));
+
+var bc = new MovemmentSystem(world);
+bc.Update(10.0f);
