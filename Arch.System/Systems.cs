a01d11cd606bed902d008df8bffd3cee4e1e967a
DrSmugleaf <drsmugleaf@gmail.com>
2023-04-15 21:49:24 -0700

    Fix doc comment

--- Arch.System/Systems.cs
+++ Arch.System/Systems.cs
@@ -103,7 +103,7 @@ public class Group<T> : ISystem<T>
     /// <summary>
     ///     Creates an instance with an array of <see cref="ISystem{T}"/>'s that will belong to this group.
     /// </summary>
-    /// <param name="systems">An <see cref="ISystem{T}"/> array.</param>
+    /// <param name="systems">An <see cref="IEnumerable{T}"/> of <see cref="ISystem{T}"/>.</param>
     public Group(IEnumerable<ISystem<T>> systems)
     {
         _systems = new List<ISystem<T>>(systems);
