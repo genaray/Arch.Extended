8fd95a9ba5e27c0a8812b85126ee5b6486c72152
Reese Schultz <contact@reese.codes>
2023-09-29 19:26:52 -0500

    Prune problematic libraries in Unity publishing

--- scripts/unity-publish.sh
+++ scripts/unity-publish.sh
@@ -8,3 +8,7 @@ dotnet restore
 mkdir -p dist/Assemblies
 
 dotnet msbuild /t:Unity -p:PublishDir=`pwd`/dist/Assemblies
+
+# Unity transitively provides the below libraries. Plus, they are heavy and redistribution-restrictive.
+# All of Linq should go hence the wildcard, but System.Threading.Tasks.Extensions is fine (MIT).
+rm `pwd`/dist/Assemblies/System.Linq* `pwd`/dist/Assemblies/System.Threading.dll
