135166cd0bcb22e0f15ff817f37295ab03e6eae5
Martin Evans <martindevans@gmail.com>
2023-11-20 22:32:46 +0000

    Added some more tests to the UnsafeStack, covering everything but `ToString`

--- Arch.LowLevel.Tests/UnsafeStackTest.cs
+++ Arch.LowLevel.Tests/UnsafeStackTest.cs
@@ -9,22 +9,171 @@ using static NUnit.Framework.Assert;
 [TestFixture]
 public class UnsafeStackTest
 {
-   
     /// <summary>
-    ///     Checks if <see cref="UnsafeStack{T}"/> is capable of adding itemss.
+    ///     Checks if <see cref="UnsafeStack{T}"/> checks for invalid capacity on construction.
+    /// </summary>
+    [Test]
+    public void UnsafeStackInvalidCapacity()
+    {
+        Throws<ArgumentOutOfRangeException>(() => new UnsafeStack<int>(-9));
+    }
+
+    /// <summary>
+    ///     Checks if <see cref="UnsafeStack{T}"/> is capable of adding items.
     /// </summary>
     [Test]
     public void UnsafeStackAdd()
     {
         using var stack = new UnsafeStack<int>(8);
+
+        That(stack.IsFull, Is.False);
+        That(stack.IsEmpty, Is.True);
+
         stack.Push(1);
         stack.Push(2);
         stack.Push(3);
-        
+
+        That(stack.IsFull, Is.False);
+        That(stack.IsEmpty, Is.False);
+
         That(stack.Count, Is.EqualTo(3));
         That(stack.Peek(), Is.EqualTo(3));
     }
-    
+
+    /// <summary>
+    ///     Checks if <see cref="UnsafeStack{T}"/> is can be converted to a span
+    /// </summary>
+    [Test]
+    public void UnsafeStackAsSpan()
+    {
+        using var stack = new UnsafeStack<int>(8);
+
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        var span = stack.AsSpan();
+
+        stack.Pop();
+        stack.Push(4);
+
+        That(span.Length, Is.EqualTo(3));
+
+        CollectionAssert.AreEqual(span.ToArray(), new[] { 1, 2, 4 });
+    }
+
+    /// <summary>
+    ///     Checks if <see cref="UnsafeStack{T}"/> is capable of adding items even past the initial capacity
+    /// </summary>
+    [Test]
+    public void UnsafeStackAddBeyondCapacity()
+    {
+        using var stack = new UnsafeStack<int>(4);
+        That(stack.Capacity, Is.EqualTo(4));
+
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        stack.Push(4);
+        That(stack.IsFull, Is.True);
+        stack.Push(5);
+        stack.Push(6);
+        stack.Push(7);
+
+        That(stack.Count, Is.EqualTo(7));
+        That(stack.Peek(), Is.EqualTo(7));
+    }
+
+    /// <summary>
+    ///     Checks if <see cref="UnsafeStack{T}.EnsureCapacity"/> expands capacity
+    /// </summary>
+    [Test]
+    public void UnsafeStackEnsureCapacityExpands()
+    {
+        using var stack = new UnsafeStack<int>(10);
+        That(stack.Capacity, Is.EqualTo(10));
+
+        stack.EnsureCapacity(11);
+
+        That(stack.Capacity, Is.GreaterThanOrEqualTo(11));
+    }
+
+    /// <summary>
+    ///     Checks if <see cref="UnsafeStack{T}.EnsureCapacity"/> cannot shrink the capacity
+    /// </summary>
+    [Test]
+    public void UnsafeStackEnsureCapacityCannotShrink()
+    {
+        using var stack = new UnsafeStack<int>(10);
+        That(stack.Capacity, Is.EqualTo(10));
+
+        stack.EnsureCapacity(1);
+
+        That(stack.Capacity, Is.EqualTo(10));
+    }
+
+    /// <summary>
+    ///     Checks if <see cref="UnsafeStack{T}.EnsureCapacity"/> can add a massive amount of new capacity
+    /// </summary>
+    [Test]
+    public void UnsafeStackEnsureCapacityExpandsALot()
+    {
+        using var stack = new UnsafeStack<int>(10);
+        That(stack.Capacity, Is.EqualTo(10));
+
+        stack.EnsureCapacity(10000);
+
+        That(stack.Capacity, Is.GreaterThanOrEqualTo(10000));
+    }
+
+    /// <summary>
+    ///     Checks if <see cref="UnsafeStack{T}.TrimExcess"/> can remove unused capacity
+    /// </summary>
+    [Test]
+    public void UnsafeStackTrimExcessShrinks()
+    {
+        using var stack = new UnsafeStack<int>(10);
+        That(stack.Capacity, Is.EqualTo(10));
+
+        stack.TrimExcess();
+
+        That(stack.Capacity, Is.LessThan(10));
+    }
+
+    /// <summary>
+    ///     Checks if <see cref="UnsafeStack{T}.TrimExcess"/> does not expand
+    /// </summary>
+    [Test]
+    public void UnsafeStackTrimExcessNeverExpands()
+    {
+        using var stack = new UnsafeStack<int>(2);
+        
+        stack.TrimExcess();
+
+        That(stack.Capacity, Is.LessThanOrEqualTo(2));
+    }
+
+    /// <summary>
+    ///     Checks if <see cref="UnsafeStack{T}"/> is capable of being cleared.
+    /// </summary>
+    [Test]
+    public void UnsafeStackClear()
+    {
+        using var stack = new UnsafeStack<int>(8);
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+
+        That(stack.Count, Is.EqualTo(3));
+        That(stack.Peek(), Is.EqualTo(3));
+
+        stack.Clear();
+
+        That(stack.Count, Is.EqualTo(0));
+        Throws<InvalidOperationException>(() => stack.Peek());
+        Throws<InvalidOperationException>(() => stack.Pop());
+    }
+
     /// <summary>
     ///     Checks if <see cref="UnsafeStack{T}"/> is capable of peeking itemss.
     /// </summary>
